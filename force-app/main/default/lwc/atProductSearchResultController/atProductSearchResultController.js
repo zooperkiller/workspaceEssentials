ba7634f865ece74674242c6b656baff4e5f4dd51
Anish Thakurta <anishthakurta@gmail.com>
2024-12-23 23:13:30 +0530

    commit 231224

--- force-app/main/default/lwc/atProductSearchResultController/atProductSearchResultController.js
+++ force-app/main/default/lwc/atProductSearchResultController/atProductSearchResultController.js
@@ -10,8 +10,39 @@ export default class AtProductSearchResultController extends LightningElement {
     @api recordId;//stores the category Id 
     @api effectiveAccountId; //stores the current user's account Id 
     @track error;
-    @track name;
+    @track error2;
+    @track name ;
     productDetail = [];
+    observer;
+    catId;
+
+    connectedCallback(){
+        // Initial ID extraction
+        this.extractIdFromUrl();
+
+        // Set up a MutationObserver to monitor changes
+       this.observer = new MutationObserver(() => {
+           this.extractIdFromUrl(); // Re-extract ID when the URL changes
+       });
+
+       // Observe the body or specific container where URL path changes
+       this.observer.observe(document.body, { childList: true, subtree: true });
+      
+   }
+   disconnectedCallback() {
+       // Disconnect the observer to prevent memory leaks
+       if (this.observer) {
+           this.observer.disconnect();
+       }
+   }
+
+   extractIdFromUrl() {
+    const pathname = window.location.pathname;
+    const segments = pathname.split('/');
+    this.catId = segments[segments.length - 1]; // Extract ID dynamically
+    console.log('Extracted ID:', this.catId);
+   }
+
 
     @wire(getRecord, { recordId: USER_ID, fields: [NAME_FIELD,USER_ACCOUNT_ID] })
          
@@ -23,29 +54,18 @@ export default class AtProductSearchResultController extends LightningElement {
         } 
         else if (data) 
         {
+            console.log('1st wire result: '+data);
+            console.log('1st wire result: '+JSON.stringify(data));
             this.name = data.fields.Name.value;
             this.effectiveAccountId = data.fields.AccountId.value;
             console.log('name:',this.name, 'accountId:',this.effectiveAccountId);
-            console.log('recordId: '+this.recordId);
             if(this.effectiveAccountId){
-                getProductDetailResult({ effectiveAccountId: this.effectiveAccountId , categId: this.recordId })
+                console.log('Account ID:',this.effectiveAccountId ,'++','Category ID:',this.catId);
                 
-                .then(result=>{
-                    
-                    console.log('result: '+result); 
-                    console.log('result: '+JSON.stringify(result)); 
-                    this.productDetail = result;  
-                    console.log('Detail result: '+this.productDetail);
-                    console.log('@@',result.productsPage.products);
-                    this.productDetail = result.productsPage.products;  
-                    
-                })
-                .catch(error=>{
-                    console.log('error: '+error);
-                })
             }
             
         }
     }
+
     
 }
