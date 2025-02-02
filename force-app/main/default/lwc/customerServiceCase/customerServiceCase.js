c8c3759e195d686f0e46300bdb954c0a6a9172e5
zooperkiller <anishthakurta@gmail.com>
2025-02-03 00:16:37 +0530

    commit 03022025 0016AM

--- force-app/main/default/lwc/customerServiceCase/customerServiceCase.js
+++ force-app/main/default/lwc/customerServiceCase/customerServiceCase.js
@@ -14,20 +14,52 @@ export default class CustomerServiceCase extends LightningElement {
     this.showModal = false;
   }
 
-  cases = [];
-    error;
-    showModal = false;
+  @track cases = [];
+  @track error;
+  @track currentPage = 1;
+  @track totalRecords = 0;
+  @track pageSize = 5; // Number of records per page
+  @track totalPages = 0;
+  @track paginatedCases = [];
 
-    @wire(getCases)
-    wiredCases({ error, data }) {
-        if (data) {
-          console.log('@@data:',data);
-            this.cases = data;
-            this.error = undefined;
-        } else if (error) {
-            this.error = error;
-            console.log('@@error000:',error);
-            this.cases = undefined;
-        }
-    }
+  @wire(getCases,{})
+  wiredCases({ error, data }) {
+      if (data) {
+          this.cases = data;
+          this.totalRecords = data.length;
+          this.totalPages = Math.ceil(this.totalRecords / this.pageSize);
+          this.updatePaginatedCases();
+      } else if (error) {
+          this.error = error;
+          this.cases = undefined;
+      }
+  }
+
+  updatePaginatedCases() {
+      const startIndex = (this.currentPage - 1) * this.pageSize;
+      const endIndex = startIndex + this.pageSize;
+      this.paginatedCases = this.cases.slice(startIndex, endIndex);
+  }
+
+  handlePrevious() {
+      if (this.currentPage > 1) {
+          this.currentPage--;
+          this.updatePaginatedCases();
+      }
+  }
+
+  handleNext() {
+      if (this.currentPage < this.totalPages) {
+          this.currentPage++;
+          this.updatePaginatedCases();
+      }
+  }
+
+  get isPreviousDisabled() {
+      return this.currentPage === 1 ? 'disabled-button' : '';
+  }
+
+  get isNextDisabled() {
+      return this.currentPage === this.totalPages ? 'disabled-button' : '';
+  }
 }
